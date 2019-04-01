4fc531d90bbea191e26970f396cbd071740ba4d1
Mang Tri Bandrex <37132315+penghancur66@users.noreply.github.com>
2019-04-02 06:58:38 +0800

    +halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,60 @@
-@extends('layouts.app')
-
+@extends('frontEnd.layouts.master')
+@section('title','Login Register Page')
+@section('slider')
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+    <div class="container">
+        @if(Session::has('message'))
+            <div class="alert alert-success text-center" role="alert">
+                {{Session::get('message')}}
+            </div>
+        @endif
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-1">
+                <div class="login-form"><!--login form-->
+                    <h2>Login to your account</h2>
+                    <form action="{{ route('login') }}" method="post" class="form-horizontal">
+                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        <input type="email" placeholder="Email" name="email"/>
+                        <input type="password" placeholder="Password" name="password"/>
+                        <span>
+                            <input type="checkbox" class="checkbox">
+                            Keep me signed in
+                        </span>
+                        <button type="submit" class="btn btn-default">Login</button>
                     </form>
-                </div>
+                </div><!--/login form-->
+            </div>
+            <div class="col-sm-1">
+                <h2 class="or">OR</h2>
+            </div>
+            <div class="col-sm-4">
+                <div class="signup-form"><!--sign up form-->
+                    <h2>New User Signup!</h2>
+                    <form action="{{ route('register') }}" method="post" class="form-horizontal">
+                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+
+                        <input type="text" placeholder="Name" name="name" value="{{old('name')}}"/>
+                        <span class="text-danger">{{$errors->first('name')}}</span>
+
+                        <input type="email" placeholder="Email Address" name="email" value="{{old('email')}}"/>
+                        <span class="text-danger">{{$errors->first('email')}}</span>
+
+                        <input type="hidden" name="profile_image" value=NULL>
+                        <input type="hidden" name="status" value=NULL>
+
+
+                        <input type="password" placeholder="Password" name="password" value="{{old('password')}}"/>
+                        <span class="text-danger">{{$errors->first('password')}}</span>
+
+                        <input type="password" placeholder="Confirm Password" name="password_confirmation" value="{{old('password_confirmation')}}"/>
+                        <span class="text-danger">{{$errors->first('password_confirmation')}}</span>
+
+                        <button type="submit" class="btn btn-default">Signup</button>
+                    </form>
+                </div><!--/sign up form-->
             </div>
         </div>
     </div>
-</div>
+    <div style="margin-bottom: 20px;"></div>
 @endsection
